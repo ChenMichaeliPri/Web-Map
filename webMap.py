00359f9aaf5bb371a16c3af783361a993541545d
ChenMichaeliPri <chenmichaeli.op@gmail.com>
2022-03-13 17:17:41 +0200

    Reading data from CSV files.

--- webMap.py
+++ webMap.py
@@ -1,9 +1,14 @@
 
 # Web Map of loved Restaurants and Bars in Tel Aviv
 import folium
+import pandas
 
 # Base layer of the map
 webMap = folium.Map(location=[32.063330, 34.772446], zoom_start=16)
+
+# Loading the data for the map from CSV files
+restaurantsData = pandas.read_csv("Restaurants.csv")
+barsData = pandas.read_csv("Bars.csv")
 
 # Saving as html file
 webMap.save("webMap.html")
