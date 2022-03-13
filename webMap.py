4e2fe5106a89df136f23a0d72f631fa0b1c83367
ChenMichaeliPri <chenmichaeli.op@gmail.com>
2022-03-13 16:22:57 +0200

    Creating a base layer of the map.

--- webMap.py
+++ webMap.py
@@ -1,3 +1,9 @@
 
 # Web Map of loved Restaurants and Bars in Tel Aviv
-import Folium
+import folium
+
+# Base layer of the map
+webMap = folium.Map(location=[32.063330, 34.772446], zoom_start=16)
+
+# Saving as html file
+webMap.save("webMap.html")
